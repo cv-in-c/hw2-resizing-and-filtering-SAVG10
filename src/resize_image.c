1e2d454f0707ad7e5c2aa24cd08b0e5226580497
SAVG10 <gvaman10@gmail.com>
2023-07-18 11:55:05 +0530

    NNI

--- src/resize_image.c
+++ src/resize_image.c
@@ -3,14 +3,27 @@
 
 float nn_interpolate(image im, float x, float y, int c)
 {
-    // TODO Fill in
-    return 0;
+    int nearX = (int)(x+0.5f);
+    int nearY = (int)(y+0.5f);
+    return get_pixel(im, nearX, nearY, c);
 }
 
 image nn_resize(image im, int w, int h)
 {
-    // TODO Fill in (also fix that first line)
-    return make_image(1,1,1);
+    image new = make_image(w, h, im.c);
+
+    float xScale = ((float)(im.w))/w;
+    float yScale = ((float)(im.h))/h;
+    for(int z=0;z<new.c;z++){
+        for(int x=0;x<new.w;x++){
+            for(int y=0;y<new.h;y++){
+                float nearestX = (float)(x + 0.5f) * xScale - 0.5f;
+                float nearestY = (float)(y + 0.5f) * yScale - 0.5f;
+                set_pixel(new, x, y, z, nn_interpolate(im, nearestX, nearestY, z));
+            }
+        }
+    }
+    return new;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
