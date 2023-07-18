cc5c034ff2134f7df1a7579b8a441c741ad4a535
SAVG10 <gvaman10@gmail.com>
2023-07-18 17:01:51 +0530

    High Pass Filter

--- src/filter_image.c
+++ src/filter_image.c
@@ -8,25 +8,112 @@
 
 void l1_normalize(image im)
 {
-    // TODO
+    for(int z = 0; z < im.c; z ++) {
+        float sumT = 0;
+        for(int y = 0; y < im.h; y ++) {
+            for(int x = 0; x < im.w; x ++) {
+                sumT = sumT+get_pixel(im, x, y, z);
+            }
+        }
+
+        if (sumT == 0) {
+        return;
+        }   
+
+        for(int y = 0; y < im.h; y ++) {
+            for(int x = 0; x < im.w; x ++) {
+                set_pixel(im, x, y, z, get_pixel(im, x, y, z) / sumT);
+            }
+        }
+    }
+    
 }
 
 image make_box_filter(int w)
 {
-    // TODO
-    return make_image(1,1,1);
+    image boxFilter = make_image(w, w, 1);
+    float num_of_pixels = ((float)w) * ((float)w);
+    for(int x = 0; x < w; x ++) {
+        for(int y = 0; y < w; y ++) {
+            set_pixel(boxFilter, y, x, 0, 1.0 / num_of_pixels);
+        }
+    }
+
+    return boxFilter;
+
+}
+
+//New to function to help out in convolve_image
+
+static float filter_value(image im, image f, int x, int y, int c, int cf)
+ {
+    float sum = 0;
+    int fy = (f.h-1)/2;
+    int fx = (f.w-1)/2; 
+
+    for (int i = 0; i < f.w; i++) {
+        for (int j = 0; j < f.h; j++) {
+            sum = sum + (get_pixel(im, x - fx +i, y - fy + j, c) * get_pixel(f, i, j, cf));
+        }
+    }
+
+    return sum;
+
 }
 
 image convolve_image(image im, image filter, int preserve)
 {
-    // TODO
-    return make_image(1,1,1);
+    image convolve = copy_image(im);
+    
+    for (int z = 0; z < im.c; z++) {
+        int filter_c = z;
+        
+        //Can reuse the filter if the image has only one channel
+
+        if(filter.c == 1) {
+            filter_c = 0;
+        }
+        
+        for (int x = 0; x < im.w; x++) {
+            for (int y = 0; y < im.h; y++) {
+                set_pixel(convolve, x, y, z, filter_value(im, filter, x, y, z, filter_c));
+            }
+        }
+
+    }
+    
+    // To get a 1 channel image
+
+    if ((!preserve) || (im.c == 1)) {
+        image sum = make_image(convolve.w, convolve.h, 1); 
+        for (int x = 0; x < im.w; x++) {
+            for (int y = 0; y < im.h; y++) {
+                float sumD = 0;
+                for(int z = 0; z < im.c; z++) {
+                    sumD = sumD +  get_pixel(convolve, x, y ,z);
+                }
+                set_pixel(sum, x, y, 0, sumD);
+            }
+        }
+        return sum;
+    }
+    return convolve;
 }
 
 image make_highpass_filter()
 {
-    // TODO
-    return make_image(1,1,1);
+     image filter = make_image(3, 3, 1);
+    set_pixel(filter, 0, 0, 0, 0);
+    set_pixel(filter, 0, 1, 0, -1);
+    set_pixel(filter, 0, 2, 0, 0);
+    set_pixel(filter, 1, 0, 0, -1);
+    set_pixel(filter, 1, 1, 0, 4);
+    set_pixel(filter, 1, 2, 0, -1);
+    set_pixel(filter, 2, 0, 0, 0);
+    set_pixel(filter, 2, 1, 0, -1);
+    set_pixel(filter, 2, 2, 0, 0);
+
+    return filter;
 }
 
 image make_sharpen_filter()
@@ -42,10 +129,10 @@ image make_emboss_filter()
 }
 
 // Question 2.2.1: Which of these filters should we use preserve when we run our convolution and which ones should we not? Why?
-// Answer: TODO
+// Answer: sharpen, emboss and highpass.(Not sure why ??)
 
 // Question 2.2.2: Do we have to do any post-processing for the above filters? Which ones and why?
-// Answer: TODO
+// Answer: Yes, we need to normalize them so that the pixel value remains less than 1 and image doesn't go outside the boundary.
 
 image make_gaussian_filter(float sigma)
 {
